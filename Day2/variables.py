592b393c2c905cd08297cfc3094b66366a85a2d3
MilvianGroup <arman@milvian.group>
2026-04-10 15:48:18 -0700

    day 2 complete

---
+++ Day2/variables.py
@@ -0,0 +1,40 @@
+#Day 2: 30 Days of pyhton programing
+first_name = 'Arman'
+last_name = 'Sahota'
+full_name = 'Arman Sahota'
+country = 'USA'
+city = 'Seattle'
+age = 20
+year = 2005
+is_true, is_light_on, is_married = True, False, False
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(age))
+print(len(first_name))
+print(len(first_name), len(last_name))
+num_one, num_two = 5, 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+radius = 30
+pi = 3.14
+area_of_circle = pi * (radius ** 2) 
+circum_of_circle = 2 * pi * radius
+print(area_of_circle, circum_of_circle)
+radius = int(input('what is the radius'))
+area_of_circle = pi * (radius ** 2) 
+circum_of_circle = 2 * pi * radius
+print(area_of_circle, circum_of_circle)
+first_name = input("first_name")
+last_name = input("last_name")
+country = input("country?")
+age = int(input('age?'))
+
+
+
+
